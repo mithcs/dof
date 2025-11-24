2b363532632ba69ab4b8b80e0f4009afd3faadaa
Mitesh H. Soni <smiteshhc@gmail.com>
2025-11-24 20:05:14 +0530

    fix: minor issues in add handler
    - also refactor add handler

--- internal/handlers/add.go
+++ internal/handlers/add.go
@@ -5,18 +5,16 @@ import (
 	"errors"
 
 	"github.com/mithcs/dof/internal/files"
-	"github.com/mithcs/dof/internal/metadata"
+	md "github.com/mithcs/dof/internal/metadata"
 	"github.com/urfave/cli/v3"
 )
 
 // AddHandler is handler for add subcommand
 func AddHandler(ctx context.Context, cmd *cli.Command) error {
-	// TODO: Cleanup
+	m := &md.Metadata{}
 	name := cmd.String("name")
-
-	m := &metadata.Metadata{}
 	entry, err := m.Get(name)
-	if err != nil && !errors.Is(err, metadata.ErrEntryNotFound) {
+	if err != nil && !errors.Is(err, md.ErrEntryNotFound) {
 		return err
 	}
 
@@ -25,39 +23,58 @@ func AddHandler(ctx context.Context, cmd *cli.Command) error {
 		return err
 	}
 
-	start := len(entry.Paths)
-	var addFile func([]string, string, int) error
-	switch {
-	case cmd.Bool("copy"):
-		addFile = files.CopyToName
-		entry.Method = metadata.Copy
-	case cmd.Bool("symlink"):
-		addFile = files.MoveAndSymlink
-		entry.Method = metadata.Symlink
-	default:
-		addFile = files.CopyToName
-		entry.Method = metadata.Copy
-	}
-
-	err = addFile(paths, name, start)
+	pathsCount := len(entry.Paths)
+	method := selectMethod(entry.Method, cmd)
+	err = addFile(method, paths, name, pathsCount)
 	if err != nil {
 		return err
 	}
 
 	paths = files.GeneralizePaths(paths)
+
 	entry.Name = name
 	entry.Paths = append(entry.Paths, paths...)
+	entry.Method = method
 
-	// if entry (we got earlier) is empty then Add() otherwise Update()
-	if start == 0 {
-		if err = m.Add(entry); err != nil {
-			return err
-		}
-	} else {
-		if err = m.Update(entry); err != nil {
-			return err
-		}
+	err = saveEntry(m, pathsCount, entry)
+	if err != nil {
+		return err
 	}
 
 	return nil
+}
+
+// selectMethod selects method to use. It uses given method if set, otherwise
+// checks flags to determine. If no flags are set either, default method is returned
+func selectMethod(method md.Method, cmd *cli.Command) md.Method {
+	if method != "" {
+		return method
+	}
+
+	switch {
+	case cmd.Bool("copy"):
+		return md.Copy
+	case cmd.Bool("symlink"):
+		return md.Symlink
+	default:
+		// select default from config
+		return md.Copy
+	}
+}
+
+func addFile(method md.Method, paths []string, name string, pathCount int) error {
+	if method == md.Symlink {
+		return files.MoveAndSymlink(paths, name, pathCount)
+	} else {
+		return files.CopyToName(paths, name, pathCount)
+	}
+}
+
+// saveEntry calls Add() method if pathsCount is 0, otherwise calls Update()
+func saveEntry(m *md.Metadata, pathsCount int, e md.Entry) error {
+	if pathsCount == 0 {
+		return m.Add(e)
+	} else {
+		return m.Update(e)
+	}
 }
